843f6b90e543040d981c84ba2078d63940a86c2c
Stackbit Code Editor <projects@stackbit.com>
2021-05-22 16:46:31 +0000

    about.js: changed

--- src/components/about.js
+++ src/components/about.js
@@ -49,14 +49,6 @@ const Header = ({ data }) => (
                     rel="noopener noreferrer"
                   ></a>
                 </li>
-                <li>
-                  <a
-                    className="fab fa-github"
-                    href={data.github}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  ></a>
-                </li>
               </ul>
             </div>
           </div>
